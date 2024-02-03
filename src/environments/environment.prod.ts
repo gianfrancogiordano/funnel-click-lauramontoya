240c8319bdd9648d5b0a3704668237b967a2005b
Gianfrano Giordano <gianfranogiordano@gmail.com>
2024-02-02 19:26:42 -0500

    api y pixel fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   base_url: 'https://api.soyclickstore.com/api/v1',
   wsUrl: 'https://api.soyclickstore.com',
   cloudFront: 'https://d3cj1y2eynyat2.cloudfront.net',
-  clickstoreId: '65b0641c4a8fd006d2765113',
-  clicstoreDomain: 'calzadodante.getcs.link',
-  pixel: '346183598232140',
-  apiFB: 'EAAMZCiP7UQcUBO2kBHgNbbCoeGEy9wM9ZBvjZACpFL6yuOyCKGeU8AMlebj5a5DBX7Y6vLfaxYtaVQwBqJBewc7Csohohq02EK8SSAffrwjvrdJugKzIxvSbzxE9EnZC7Rhx8CEWbE0N95yLAY8R5S9liZA64jpbD5YkzsaZBwwkBRXNMKlZCV8XO3ZAxmkSsBReNwZDZD'
+  clickstoreId: '639c8ee5eb5d3c02c08b29da',
+  clicstoreDomain: 'https://shop.lauramontoyashoes.com',
+  pixel: '446036844890135',
+  apiFB: 'EAAFuKLBMDXsBO16VfvGmpND21CUdPl0MQFjjxaZB0dHl4B0LJftWTeDAKH9rTzcfC9M6yZCqUo5PNWcADUFJr5NO7U6jFIY1mDLAivL4MPql0tcr0Wx6drkpCjp51IfgPX8tA3W8rvHCLRaKL0GZADFvJyhldb7uV3UMLCwclQ8z38ZCHRRuIXwsPNNadBda3wZDZD'
 };
